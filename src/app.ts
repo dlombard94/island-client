db5bda77bf10275f64af70b88f04c4048748cd97
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-05-17 17:48:52 +0100

    added router

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'review'],
+        name: 'Review',
+        moduleId: PLATFORM.moduleName('views/review'),
+        nav: true,
+        title: 'Review'
+      },
+      {
+        route: 'islands',
+        name: 'islands',
+        moduleId: PLATFORM.moduleName('views/islands'),
+        nav: true,
+        title: 'Island'
+      }
+    ]);
+    this.router = router;
+  }
 }
